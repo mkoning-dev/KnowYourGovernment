0362687acce2c4bd3cf4de8e55c7647d541875b9
martijnkoning <mkoning@hawk.iit.edu>
2020-03-28 23:37:53 -0500

    Added location detection feature. This will be revised.

--- app/src/main/java/com/martijnkoning/knowyourgovernment/MainActivity.java
+++ app/src/main/java/com/martijnkoning/knowyourgovernment/MainActivity.java
@@ -2,20 +2,40 @@ package com.martijnkoning.knowyourgovernment;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
+
+    private static int MY_LOCATION_REQUEST_CODE_ID = 123;
+    private LocationManager locationManager;
+    private Criteria criteria;
 
     private final List<Official> officialList = new ArrayList<>();
 
@@ -27,6 +47,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        criteria = new Criteria();
+        criteria.setPowerRequirement(Criteria.POWER_HIGH);
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setSpeedRequired(false);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    },
+                    MY_LOCATION_REQUEST_CODE_ID);
+        } else {
+            setLocation();
+        }
 
         recyclerView = findViewById(R.id.RecyclerView);
 
@@ -75,5 +117,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void setLocation() {
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+        Location currentLocation = locationManager.getLastKnownLocation(Objects.requireNonNull(locationManager.getBestProvider(criteria, true)));
+
+        if (currentLocation != null) {
+            try {
+                List<Address> addresses;
+                addresses = geocoder.getFromLocation(currentLocation.getLatitude(), currentLocation.getLongitude(), 1);
+
+                String city = addresses.get(0).getLocality();
+                String state = addresses.get(0).getAdminArea();
+                String zip = addresses.get(0).getPostalCode();
+
+                String address = city + ", " + state + " " + zip;
+
+                ((TextView) findViewById(R.id.loc_address)).setText(address);
+            } catch (IOException e) {
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
+        } else {
+            ((TextView) findViewById(R.id.loc_address)).setText(R.string.no_locs);
+        }
+
     }
 }
